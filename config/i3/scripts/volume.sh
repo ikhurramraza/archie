b2581d9106511752018c7f65de94f16ef3222c6b
Khurram Raza <ikhurramraza@gmail.com>
2022-09-14 19:51:49 +0200

    Fix volume script for multiple devices
    When bluetooth device is connected we get two sinks and we should
    pick the one which is "running"

--- config/i3/scripts/volume.sh
+++ config/i3/scripts/volume.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-pactl_output="$(pactl list sinks)"
+pactl_output="$(pactl list sinks | grep -A10 -P 'State: RUNNING')"
 
 volume="$(echo "$pactl_output" | tr ' ' '\n' | grep -m1 '%')"
 label="$(echo "$pactl_output" | awk '/Mute/ { print $2 }' | sed -e 's/no/󰕾/' | sed -e 's/yes/󰸈/')"
