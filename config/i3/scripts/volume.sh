4d8fc105720f4f3c75832aee24fae0e56092a39a
Khurram Raza <ikhurramraza@gmail.com>
2022-09-27 18:37:10 +0200

    Fix i3 bar volume script

--- config/i3/scripts/volume.sh
+++ config/i3/scripts/volume.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-pactl_output="$(pactl list sinks | grep -A10 -P 'State: RUNNING')"
+pactl_output="$(pactl list sinks | grep -P "^\s*Volume|^\s*Mute" | tail -n2)"
 
 volume="$(echo "$pactl_output" | tr ' ' '\n' | grep -m1 '%')"
 label="$(echo "$pactl_output" | awk '/Mute/ { print $2 }' | sed -e 's/no/󰕾/' | sed -e 's/yes/󰸈/')"
