7ffedab1eb2093b7363194eecb0b355f0dc9b778
Khurram Raza <ikhurramraza@gmail.com>
2023-02-08 17:05:10 +0100

    feat(nvim): add key mapping for closing all buffers

--- config/nvim/lua/keys.lua
+++ config/nvim/lua/keys.lua
@@ -1,6 +1,7 @@
 -- Save and exit keys
 vim.keymap.set("n", "<C-S>", ":w<CR>", { silent = true })
 vim.keymap.set("n", "<C-Q>", ":Bwipeout<CR>", { silent = true })
+vim.keymap.set("n", "<C-W>", ":bufdo bw<CR>", { silent = true })
 
 -- Switch buffers
 vim.keymap.set("n", "]b", ":bn<CR>", { silent = true })
