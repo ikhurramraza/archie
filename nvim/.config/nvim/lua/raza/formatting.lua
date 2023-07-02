50de8f2c75815524851663f97de8f74eb6e95fe8
Khurram Raza <ikhurramraza@gmail.com>
2023-07-02 13:29:17 +0200

    feat(nvim): use dynamic commands for ruby formatters

--- nvim/.config/nvim/lua/raza/formatting.lua
+++ nvim/.config/nvim/lua/raza/formatting.lua
@@ -1,11 +1,38 @@
 local null_ls = require("null-ls")
 local helpers = require("null-ls.helpers")
 
+-- Ruby bundle prefixed dynamic commands
+-- For Ruby apps managed by bundler, we need to use bundle exec command
+-- instead of simply using the command. Hence, we need to set the command
+-- dynamically and it needs to be cached to avoid directory exists calls
+-- on the subsequent runs for better performance.
+
+local bundle_prefix_cache = {}
+
+local function bundle_prefixed_dynamic_command(command, cache_key)
+  return function(params)
+    if bundle_prefix_cache[cache_key] then
+      return bundle_prefix_cache[cache_key]
+    end
+
+    local Path = require("plenary.path")
+    local vendor_bundle_path = Path:new(params.root .. "/vendor/bundle")
+
+    if vendor_bundle_path:is_dir() then
+      bundle_prefix_cache[cache_key] = { "bundle", "exec", command }
+    else
+      bundle_prefix_cache[cache_key] = command
+    end
+
+    return bundle_prefix_cache[cache_key]
+  end
+end
+
 -- Diagnostics
 
 local bundled_rubocop_diagnostics = null_ls.builtins.diagnostics.rubocop.with({
-  command = "bundle",
-  args = { "exec", "rubocop", "-fj", "--force-exclusion", "-s", "$FILENAME" },
+  dynamic_command = bundle_prefixed_dynamic_command("rubocop", "bundled_rubocop_diagnostics"),
+  args = { "-fj", "--force-exclusion", "-s", "$FILENAME" },
 })
 
 -- Formatters
@@ -15,7 +42,7 @@ local stree_formatter = {
   method = null_ls.methods.FORMATTING,
   filetypes = { "ruby" },
   generator = helpers.formatter_factory({
-    command = "bundle",
+    dynamic_command = bundle_prefixed_dynamic_command("stree", "stree_formatter"),
     args = function(params)
       local bufnr = vim.fn.bufnr()
       local filename = string.sub(params.bufname, #params.root + 2)
@@ -24,14 +51,14 @@ local stree_formatter = {
         vim.cmd.w()
       end
 
-      return { "exec", "stree", "format", filename }
+      return { "format", filename }
     end,
   }),
 }
 
 local bundled_rubocop_formatter = null_ls.builtins.formatting.rubocop.with({
-  command = "bundle",
-  args = { "exec", "rubocop", "-fq", "-a", "--stderr", "-s", "$FILENAME" },
+  dynamic_command = bundle_prefixed_dynamic_command("rubocop", "bundled_rubocop_formatter"),
+  args = { "-fq", "-a", "--stderr", "-s", "$FILENAME" },
 })
 
 return {
