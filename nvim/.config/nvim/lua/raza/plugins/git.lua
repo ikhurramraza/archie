a7f7341143c4c8ee7eec204c663d30b175b5e6dc
Khurram Raza <ikhurramraza@gmail.com>
2023-07-07 19:52:40 +0200

    refactor(nvim): cleanup git plugins definition

--- nvim/.config/nvim/lua/raza/plugins/git.lua
+++ nvim/.config/nvim/lua/raza/plugins/git.lua
@@ -1,59 +1,59 @@
-local NeoGit = {
-  "NeogitOrg/neogit",
-  cmd = "Neogit",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "sindrets/diffview.nvim", cmd = "DiffviewFileHistory" },
+return {
+  {
+    "NeogitOrg/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "sindrets/diffview.nvim", cmd = "DiffviewFileHistory" },
+    },
+    opts = {
+      kind = "replace",
+      preview_buffer = { kind = "floating" },
+      commit_popup = { kind = "floating" },
+      integrations = { diffview = true },
+    },
   },
-  opts = {
-    kind = "replace",
-    preview_buffer = { kind = "floating" },
-    commit_popup = { kind = "floating" },
-    integrations = { diffview = true },
-  },
-}
 
-local GitBlame = {
-  "f-person/git-blame.nvim",
-  cmd = "GitBlameToggle",
-  config = function()
-    vim.g.gitblame_enabled = 0
-  end,
-  keys = {
-    {
-      "<leader>ob",
-      vim.cmd.GitBlameToggle,
-      silent = true,
-      desc = "Toggle git blame",
+  {
+    "f-person/git-blame.nvim",
+    cmd = "GitBlameToggle",
+    config = function()
+      vim.g.gitblame_enabled = 0
+    end,
+    keys = {
+      {
+        "<leader>ob",
+        vim.cmd.GitBlameToggle,
+        silent = true,
+        desc = "Toggle git blame",
+      },
+    },
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "VeryLazy",
+    opts = {
+      on_attach = function(bufnr)
+        local gs = require("gitsigns")
+
+        vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr })
+      end,
+    },
+  },
+
+  {
+    "tyru/open-browser-github.vim",
+    dependencies = "tyru/open-browser.vim",
+    keys = {
+      { "<leader>ghf", "<CMD>OpenGithubFile<CR>", silent = true, desc = "Open current file on Github" },
+      { "<leader>ghp", "<CMD>OpenGithubPullReq<CR>", silent = true, desc = "Open current PR on Github" },
     },
   },
 }
-
-local GitSigns = {
-  "lewis6991/gitsigns.nvim",
-  event = "VeryLazy",
-  opts = {
-    on_attach = function(bufnr)
-      local gs = require("gitsigns")
-
-      vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
-      vim.keymap.set("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr })
-    end,
-  },
-}
-
-local GithubOpen = {
-  "tyru/open-browser-github.vim",
-  dependencies = "tyru/open-browser.vim",
-  keys = {
-    { "<leader>ghf", "<CMD>OpenGithubFile<CR>", silent = true, desc = "Open current file on Github" },
-    { "<leader>ghp", "<CMD>OpenGithubPullReq<CR>", silent = true, desc = "Open current PR on Github" },
-  },
-}
-
-return { GitBlame, GitSigns, GithubOpen, NeoGit }
