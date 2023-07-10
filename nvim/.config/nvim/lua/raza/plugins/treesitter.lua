7f14b36f68fef9936408d3fab383502d6244a0e6
Khurram Raza <ikhurramraza@gmail.com>
2023-07-10 20:14:49 +0200

    feat(nvim): introduce treesitter playground plugin

--- nvim/.config/nvim/lua/raza/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/raza/plugins/treesitter.lua
@@ -1,28 +1,40 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  build = ":TSUpdate",
-  main = "nvim-treesitter.configs",
-  opts = {
-    highlight = {
-      enable = true,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+      highlight = {
+        enable = true,
+      },
+      ensure_installed = {
+        "bash",
+        "html",
+        "ini",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "ruby",
+        "rust",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
     },
-    ensure_installed = {
-      "bash",
-      "html",
-      "ini",
-      "javascript",
-      "json",
-      "lua",
-      "make",
-      "ruby",
-      "rust",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
+  },
+
+  {
+    "nvim-treesitter/playground",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    cmd = {
+      "TSHighlightCapturesUnderCursor",
+      "TSNodeUnderCursor",
+      "TSPlaygroundToggle",
     },
   },
 }
