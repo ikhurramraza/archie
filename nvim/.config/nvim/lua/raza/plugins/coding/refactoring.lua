9d7aa8e32cea2ab255987868ab0ab1867f16b6bd
Khurram Raza <ikhurramraza@gmail.com>
2023-07-18 10:35:00 +0200

    fix(nvim): fix refactoring plugin definition
    It was too nested and the plugin manager was having issue seeing it.

--- nvim/.config/nvim/lua/raza/plugins/coding/refactoring.lua
+++ nvim/.config/nvim/lua/raza/plugins/coding/refactoring.lua
@@ -11,16 +11,14 @@ local function key(key, mode, refactor)
 end
 
 return {
-  {
-    "ThePrimeagen/refactoring.nvim",
-    keys = {
-      key("<leader>rB", "n", "Extract Block To File"),
-      key("<leader>rF", "x", "Extract Function To File"),
-      key("<leader>rb", "n", "Extract Block"),
-      key("<leader>rf", "x", "Extract Function"),
-      key("<leader>ri", "n", "Inline Variable"),
-      key("<leader>ri", "x", "Inline Variable"),
-      key("<leader>rv", "x", "Extract Variable"),
-    },
+  "ThePrimeagen/refactoring.nvim",
+  keys = {
+    key("<leader>rB", "n", "Extract Block To File"),
+    key("<leader>rF", "x", "Extract Function To File"),
+    key("<leader>rb", "n", "Extract Block"),
+    key("<leader>rf", "x", "Extract Function"),
+    key("<leader>ri", "n", "Inline Variable"),
+    key("<leader>ri", "x", "Inline Variable"),
+    key("<leader>rv", "x", "Extract Variable"),
   },
 }
