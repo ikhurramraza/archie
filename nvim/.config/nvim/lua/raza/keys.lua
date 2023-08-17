ca48903edab4109a6617a1a6296e230c91c2612e
Khurram Raza <ikhurramraza@gmail.com>
2023-08-17 03:06:59 +0200

    feat(nvim): add missing keymap descriptions

--- nvim/.config/nvim/lua/raza/keys.lua
+++ nvim/.config/nvim/lua/raza/keys.lua
@@ -19,7 +19,7 @@ vim.keymap.set("n", "<leader>cf", ":let @+=@%<CR>", { silent = true })
 vim.keymap.set("n", "<leader>cl", ":let @+=@% . ':' . line('.')<CR>", { silent = true })
 
 -- Replace current word
-vim.keymap.set("n", "<leader>rw", ":%s/<C-r><C-w>")
+vim.keymap.set("n", "<leader>rw", ":%s/<C-r><C-w>", { silent = true, desc = "Replace current word" })
 
 -- Diagnostics
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { silent = true, desc = "Next diagnostic" })
@@ -27,20 +27,20 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { silent = true, desc = "Pre
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { silent = true, desc = "Open diagnostics window" })
 
 -- Copy to clipboard
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>y", '"+y', { silent = true, desc = "Yank to system clipboard" })
+vim.keymap.set("v", "<leader>y", '"+y', { silent = true, desc = "Yank to system clipboard" })
 
 -- Move to start/end of line in insert mode
 vim.keymap.set("i", "<C-A>", "<C-O>_")
 vim.keymap.set("i", "<C-E>", "<C-O>$")
 
 -- Paste from clipboard
-vim.keymap.set("n", "<leader>p", '"+p')
-vim.keymap.set("n", "<leader>P", '"+P')
+vim.keymap.set("n", "<leader>p", '"+p', { silent = true, desc = "Paste from system clipboard" })
+vim.keymap.set("n", "<leader>P", '"+P', { silent = true, desc = "Paste from system clipboard" })
 
 -- Delete without updating the registers
-vim.keymap.set("n", "<leader>d", '"_d')
-vim.keymap.set("v", "<leader>d", '"_d')
+vim.keymap.set("n", "<leader>d", '"_d', { silent = true, desc = "Delete without registers" })
+vim.keymap.set("v", "<leader>d", '"_d', { silent = true, desc = "Delete without registers" })
 
 -- Exit terminal mode with double escape
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
