4cb38679e208ff362f99422f7e9cd150b4bf573c
Khurram Raza <ikhurramraza@gmail.com>
2023-09-28 22:15:56 +0200

    feat(nvim): run test commands in tmux
    Since all the projects use the same tmux structure of having a window
    named "shell" which is used to run tests (among other things), using
    this automates that.

--- nvim/.config/nvim/lua/raza/plugins/test.lua
+++ nvim/.config/nvim/lua/raza/plugins/test.lua
@@ -1,19 +1,19 @@
+function TmuxStrategy(command)
+  vim.fn.execute("silent !tmux send-keys -t shell '" .. command .. "' Enter")
+  vim.fn.execute("silent !tmux select-window -t shell")
+end
+
 return {
   "vim-test/vim-test",
   config = function()
-    vim.g["test#strategy"] = "basic"
+    vim.g["test#strategy"] = "tmux"
+    vim.g["test#custom_strategies"] = { tmux = TmuxStrategy }
   end,
   keys = {
-    { "<leader>tn", "<CMD>TestNearest<CR>", silent = true, desc = "Test nearest using basic strategy" },
-    { "<leader>tf", "<CMD>TestFile<CR>", silent = true, desc = "Test file using basic strategy" },
-    { "<leader>ta", "<CMD>TestSuite<CR>", silent = true, desc = "Test all using basic strategy" },
-    { "<leader>tl", "<CMD>TestLast<CR>", silent = true, desc = "Test last using basic strategy" },
-    { "<leader>tv", "<CMD>TestVisit<CR>", silent = true, desc = "Test visit using basic strategy" },
-
-    { "<leader>tN", "<CMD>TestNearest -strategy=neovim<CR>", desc = "Test nearest using neovim strategy" },
-    { "<leader>tF", "<CMD>TestFile    -strategy=neovim<CR>", desc = "Test file using neovim strategy" },
-    { "<leader>tA", "<CMD>TestSuite   -strategy=neovim<CR>", desc = "Test all using neovim strategy" },
-    { "<leader>tL", "<CMD>TestLast    -strategy=neovim<CR>", desc = "Test last using neovim strategy" },
-    { "<leader>tV", "<CMD>TestVisit   -strategy=neovim<CR>", desc = "Test visit using neovim strategy" },
+    { "<leader>tn", "<CMD>TestNearest<CR>", silent = true, desc = "Test nearest using default strategy" },
+    { "<leader>tf", "<CMD>TestFile<CR>", silent = true, desc = "Test file using default strategy" },
+    { "<leader>ta", "<CMD>TestSuite<CR>", silent = true, desc = "Test all using default strategy" },
+    { "<leader>tl", "<CMD>TestLast<CR>", silent = true, desc = "Test last using default strategy" },
+    { "<leader>tv", "<CMD>TestVisit<CR>", silent = true, desc = "Test visit using default strategy" },
   },
 }
