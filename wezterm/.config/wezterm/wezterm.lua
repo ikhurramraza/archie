3599e854f4cb90eaa4d91116069d9b1bef55196a
Khurram Raza <ikhurramraza@gmail.com>
2024-01-31 01:15:42 +0100

    feat(wezterm): disable missing glyph icon warning
    It is  always possible that  our version of Nerd  Font is missing  a new
    icon and  this leads  to a  desktop notification  from Wezterm  which we
    would rather not have.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -26,6 +26,8 @@ local M = {
   hide_tab_bar_if_only_one_tab = true,
   window_decorations = env.os == "Darwin" and "TITLE|RESIZE" or "NONE",
 
+  warn_about_missing_glyphs = false,
+
   keys = {
     { key = "f", mods = "SHIFT|CTRL", action = wezterm.action.ToggleFullScreen },
 
