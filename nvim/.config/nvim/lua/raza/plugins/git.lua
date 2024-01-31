2e4b2800179bddcc4b73b0c9d9a7157309b78ac6
Khurram Raza <ikhurramraza@gmail.com>
2024-01-31 17:13:12 +0100

    feat(nvim): add key for inline git hunk preview

--- nvim/.config/nvim/lua/raza/plugins/git.lua
+++ nvim/.config/nvim/lua/raza/plugins/git.lua
@@ -54,6 +54,7 @@ return {
         vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr })
         vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hP", gs.preview_hunk_inline, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
         vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
         vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
