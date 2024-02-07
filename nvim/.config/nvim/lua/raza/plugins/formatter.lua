f8ccf73bcba7a7626adadee30d19248e7333e3f2
Khurram Raza <ikhurramraza@gmail.com>
2024-02-08 00:51:22 +0100

    refactor(nvim): move bundler to its own module

--- nvim/.config/nvim/lua/raza/plugins/formatter.lua
+++ nvim/.config/nvim/lua/raza/plugins/formatter.lua
@@ -1,37 +1,13 @@
+local bundler = require("raza.bundler")
+
 --- Boolean flag that controls the auto formatting on save
 --- @type boolean
 local auto_format = false
 
---- Cache for bundle_command function
---- @type string | nil
-local bundle_command_prefix_cache = nil
-
---- Helper method that converts a command to a bundler command
---- based on the the presence of "vendor/bundle" directory in
---- the project root directory.
---- @param command string
---- @return string
-local function bundle_command(command)
-  if bundle_command_prefix_cache then
-    return bundle_command_prefix_cache .. command
-  end
-
-  local Path = require("plenary.path")
-  local vendor_bundle_path = Path:new(vim.fn.getcwd() .. "/vendor/bundle")
-
-  if vendor_bundle_path:is_dir() then
-    bundle_command_prefix_cache = "bundle exec "
-  else
-    bundle_command_prefix_cache = ""
-  end
-
-  return bundle_command(command)
-end
-
 --- Formatter definition for syntax tree
 local stree = function()
   return {
-    exe = bundle_command("stree"),
+    exe = bundler.bundle_command("stree"),
     args = { "format" },
     stdin = true,
   }
@@ -44,7 +20,7 @@ local rubocop = function()
   local util = require("formatter.util")
 
   return {
-    exe = bundle_command("rubocop"),
+    exe = bundler.bundle_command("rubocop"),
     stdin = true,
     no_append = true,
     args = {
