439b7a738e6518a62cecff07acb8537741bd749f
Khurram Raza <ikhurramraza@gmail.com>
2024-02-24 21:00:58 +0100

    feat(nvim): update none-ls plugin
    Some of the builtins have been marked as deprecated which have been now
    removed.

--- nvim/.config/nvim/lua/raza/plugins/none.lua
+++ nvim/.config/nvim/lua/raza/plugins/none.lua
@@ -20,20 +20,12 @@ return {
     local builtins = require("null-ls").builtins
     local customs = require("raza.none").customs
 
-    local is_not_mac = function()
-      return vim.loop.os_uname().sysname ~= "Darwin"
-    end
-
     return {
       sources = {
-        builtins.code_actions.eslint.with({ condition = is_not_mac }),
-        builtins.diagnostics.eslint.with({ condition = is_not_mac }),
         builtins.diagnostics.zsh,
         builtins.formatting.prettier,
-        builtins.formatting.rustfmt,
         builtins.formatting.shfmt,
         builtins.formatting.stylua,
-        builtins.formatting.taplo,
         builtins.hover.dictionary,
         customs.diagnostics.rubocop,
         customs.formatting.sql_formatter,
