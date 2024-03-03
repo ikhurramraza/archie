d51cd43a8ca2a7cfb108fc448fad423f63cf7e8e
Khurram Raza <ikhurramraza@gmail.com>
2024-03-03 01:52:39 +0100

    feat(nvim): enable auto save by default

--- nvim/.config/nvim/lua/raza/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/raza/plugins/auto-save.lua
@@ -2,7 +2,7 @@ return {
   "907th/vim-auto-save",
   cmd = "AutoSaveToggle",
   init = function()
-    vim.g.auto_save = 0
+    vim.g.auto_save = 1
     vim.g.auto_save_silent = 1
   end,
   keys = {
