6a74384828587137fd2facf665aabbf74abd9e12
Khurram Raza <ikhurramraza@gmail.com>
2024-03-09 15:40:53 +0100

    refactor(nvim): sort gitsigns keymaps

--- nvim/.config/nvim/lua/raza/plugins/git.lua
+++ nvim/.config/nvim/lua/raza/plugins/git.lua
@@ -53,13 +53,14 @@ return {
 
         vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr })
         vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
+        vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hb", gs.blame_line, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hP", gs.preview_hunk_inline, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
         vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
         vim.keymap.set("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
       end,
     },
   },
