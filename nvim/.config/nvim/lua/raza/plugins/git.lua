f46e9ab5173111b6cf5c1a9f9aff0e273ff51d85
Khurram Raza <ikhurramraza@gmail.com>
2024-03-11 11:18:33 +0100

    feat(nvim): add descriptions for gitsigns keys

--- nvim/.config/nvim/lua/raza/plugins/git.lua
+++ nvim/.config/nvim/lua/raza/plugins/git.lua
@@ -50,17 +50,17 @@ return {
     opts = {
       on_attach = function(bufnr)
         local gs = require("gitsigns")
+        local set = vim.keymap.set
 
-        vim.keymap.set("n", "[h", gs.prev_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "]h", gs.next_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hb", gs.blame_line, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hP", gs.preview_hunk_inline, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr })
+        set("n", "[h", gs.prev_hunk, { buffer = bufnr, desc = "Previous git change hunk" })
+        set("n", "]h", gs.next_hunk, { buffer = bufnr, desc = "Next git change hunk" })
+        set("n", "<leader>hb", gs.blame_line, { buffer = bufnr, desc = "Show git blame for current line" })
+        set("n", "<leader>hp", gs.preview_hunk, { buffer = bufnr, desc = "Preview git change hunk" })
+        set("n", "<leader>hP", gs.preview_hunk_inline, { buffer = bufnr, desc = "Preview git change hunk inline" })
+        set("n", "<leader>hr", "<cmd>Gitsigns reset_hunk<CR>", { buffer = bufnr, desc = "Reset git change hunk" })
+        set("n", "<leader>hR", gs.reset_buffer, { buffer = bufnr, desc = "Reset git changes for the buffer" })
+        set("n", "<leader>hs", "<cmd>Gitsigns stage_hunk<CR>", { buffer = bufnr, desc = "Stage git change hunk" })
+        set("n", "<leader>hS", gs.stage_buffer, { buffer = bufnr, desc = "Stage git changes for the buffer" })
       end,
     },
   },
