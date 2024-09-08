7aedb031b4a4859095a0020122a53005b0bc84a3
Khurram Raza <ikhurramraza@gmail.com>
2024-09-08 20:19:57 +0100

    fix(nvim): fix nvim-nu plugin updates
    Building nvim-nu plugin fails. The reason is unknown, we might not need
    to install `nu` command so we are skipping it.

--- nvim/.config/nvim/lua/raza/plugins/languages.lua
+++ nvim/.config/nvim/lua/raza/plugins/languages.lua
@@ -4,9 +4,6 @@ return {
     "LhKipp/nvim-nu",
     ft = "nu",
     opts = { use_lsp_features = false },
-    build = function()
-      vim.cmd.TSInstallSync("nu")
-    end,
     dependencies = "nvim-treesitter/nvim-treesitter",
   },
 }
