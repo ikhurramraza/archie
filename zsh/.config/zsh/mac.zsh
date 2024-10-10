06645c05066c40f8f155e6e08494e8f32ef4df1b
Khurram Raza <ikhurramraza@gmail.com>
2024-10-10 13:44:09 +0100

    fix(zsh): fix hard-coded home path prefix

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -4,7 +4,7 @@
 PATH="/opt/homebrew/bin:$PATH"
 
 # Add pip user install location to PATH
-PATH="$PATH:/Users/raza/.local/bin"
+PATH="$PATH:/Users/$USER/.local/bin"
 
 # Add Apple helper paths (removed from /etc/zprofile)
 PATH="$PATH:/usr/local/bin"
