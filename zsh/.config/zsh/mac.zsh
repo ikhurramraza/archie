c49d96037ee66938210c0e3069651bca998852d3
Khurram Raza <khurram.raza@doctolib.com>
2025-08-12 00:05:28 +0200

    feat(zsh): enable concurrent download on Homebrew

--- zsh/.config/zsh/mac.zsh
+++ zsh/.config/zsh/mac.zsh
@@ -9,3 +9,6 @@ PATH="$PATH:/Users/$USER/.local/bin"
 # Add Apple helper paths (removed from /etc/zprofile)
 PATH="$PATH:/usr/local/bin"
 PATH="$PATH:/Library/Apple/usr/bin"
+
+# Enable concurrent brew downloads
+export HOMEBREW_DOWNLOAD_CONCURRENCY=auto
